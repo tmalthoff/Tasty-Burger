d3e50e67a5327ef3b9e89e6ab94b45df3992e3c7
Thomas Althoff <matthewalthoff1@gmail.com>
2020-10-22 14:54:46 -0500

    Added handlebars and also part of the script

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,49 @@
+const express = require("express");
+
+const router = express.Router();
+
+let burger = require("../models/burger.js");
+
+
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    let hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  let condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.sleepy
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
+
+
+module.exports = router;
