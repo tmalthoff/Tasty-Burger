44d5ca5cd6097e1952eef6dbdb4932420998085b
Thomas Althoff <matthewalthoff1@gmail.com>
2020-10-22 13:11:17 -0500

    Working on DB

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mr Tasty", "0");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Giganto", "0");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Famished", "0");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Budget Breaker", "0");
