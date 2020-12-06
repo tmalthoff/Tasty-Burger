afa894c5b3c9e218ba3949ee886845ae8bbbf005
Thomas Althoff <matthewalthoff1@gmail.com>
2020-12-06 15:43:45 -0600

    Fix database connection

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,16 @@ var mysql = require("mysql");
 require("dotenv").config();
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.MYSQL,
+    database: "burger_db"
+  });
 }
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.MYSQL,
-  database: "burger_db"
-});
 
 // Make connection.
 connection.connect(function(err) {
